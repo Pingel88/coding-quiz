cb88a87d550d8102de89c53613bc7c417cee537b
Michael Pingel <mdpingel@gmail.com>
2021-01-15 09:57:36 -0800

    add javascript to get question 2 running as intended, only accepts orange as a favorite color otherwise pops up an alert

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,9 @@
 $(document).ready(function() {
   $("form#codeQuiz").submit(function(event) {
     const age = parseInt($("input#age").val());
+    const color = ($("input#favoriteColor").val());
     let ageResult;
+    let colorResult;
     if (age <= 18) {
       ageResult = 1;
     } else if (age > 18 && age <= 25) {
@@ -13,14 +15,20 @@ $(document).ready(function() {
     } else {
       alert("Please enter a number for your age!")
     }
+    if (color === "Orange" || color === "orange") {
+      colorResult = 1;
+    } else {
+      alert("Orange is the only true color!")
+    }
 
+     
 
 
 
 // Modify to show quiz results
     // $("#rate").text(quote);
     // $("#quote").show();
-    alert(ageResult)
+    alert(ageResult + colorResult);
     event.preventDefault();
   });
 });
