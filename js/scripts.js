d5a66541e046c2806377db6cebf166bbb6b6ef9a
Michael Pingel <mdpingel@gmail.com>
2021-01-15 10:48:43 -0800

    add javascript to get all questions working as intended, fix html form id and label in bicycle question

--- js/scripts.js
+++ js/scripts.js
@@ -6,16 +6,17 @@ $(document).ready(function() {
     const bear = parseInt($("input:radio[name=bear]:checked").val());
     const covet = parseInt($("input:radio[name=covet]:checked").val());
     const cloud = parseInt($("input:radio[name=cloud]:checked").val());
+    const bicycle = parseInt($("input:radio[name=bicycle]:checked").val());
 
     let points = 0;
     if (age <= 18) {
       points += 1;
     } else if (age > 18 && age <= 25) {
-      points += 2;
-    } else if (age > 25 && age <= 35) {
       points += 3;
-    } else if (age > 35) {
+    } else if (age > 25 && age <= 35) {
       points += 4;
+    } else if (age > 35) {
+      points += 2;
     } else {
       alert("Please enter a number for your age!")
     }
@@ -23,10 +24,11 @@ $(document).ready(function() {
     if (color === "Orange" || color === "orange") {
       points += 1;
     } else {
-      alert("Orange is the only true color!")
+      points + 100;
     }
 
-    points += bear + covet + cloud;
+    points += bear + covet + cloud
+    points *= bicycle;
     
 
      
